eb45a94d3be81762b032f3a155a3e9d872fccb25
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-07-19 09:30:05 +0600

    upload
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload
    
    upload

--- README.md
+++ README.md
@@ -1,11 +1,64 @@
-# Soulfly: A web messenger application built by the MERN stack
+![](preview.gif)
 
-The server side codebase for [Soulfly](https://soulfly.netlify.app/).
+# tide: A minimal Transmission CLI Client
 
-[Client side codebase](https://github.com/salman-abedin/soulfly-client)
+tide is a minimal TUI for the BitTorrent client [Transmission](https://transmissionbt.com/)
 
-# Features
+## Features
 
--  Members join/leave notification
--  Message read receipt
--  Typing notification
+-  Pausing torrents
+-  Starting torrents
+-  Deleting torrents
+
+## Dependencies
+
+-  **transmission-cli**
+
+-  stty, head, read, printf, echo, cut, seq, cat
+
+## Installation
+
+```sh
+git clone https://github.com/salman-abedin/tide.git && cd tide && sudo make install
+```
+
+## Uninstallation
+
+```sh
+sudo make uninstall
+```
+
+## TODOs
+
+-  Prevent glitches (lol)
+-  Scrolling support
+-  Magnet Adding
+
+---
+
+## Repos you might be interested in
+
+[bolt](https://github.com/salman-abedin/bolt)
+: The lightning fast workflow creator
+
+[uniblocks](https://github.com/salman-abedin/uniblocks)
+: The status bar wrapper
+
+[puri](https://github.com/salman-abedin/puri)
+: minimal URL launcher
+
+[crystal](https://github.com/salman-abedin/crystal)
+: The transparent setup
+
+[devour](https://github.com/salman-abedin/devour)
+: Terminal swallowing
+
+[Magpie](https://github.com/salman-abedin/magpie)
+: The dotfiles
+
+[Alfred](https://github.com/salman-abedin/alfred)
+: The scripts
+
+## Contact
+
+SalmanAbedin@disroot.org
--- tide.sh
+++ tide.sh
@@ -1,82 +1,132 @@
 #!/bin/sh
+#
+# Minimal Transmission TUI
+# Dependencies: stty, head, read, printf, echo, cut, seq, cat
 
-SHOWCURSOR="\033[?25h"
-HIDECURSOR="\033[?25l"
-ENABLEWRAP="\033[?7h"
-DISABLEWRAP="\033[?7l"
-CLEAR="\033[2J\033[H"
-cursor=1
-items=$(transmission-remote -l | sed '1d;$d' | wc -l)
-
-getkey() {
-    stty -icanon -echo
-    dd bs=1 count=1 2> /dev/null
-    stty icanon echo
-}
+cursor=/tmp/tide_cursor
+mark=/tmp/tide_mark
 
 quit() {
-    printf "%b" "$CLEAR$SHOWCURSOR"
-    kill -9 $(pgrep -f "$0")
+    printf "\033[?7h\033[?25h\033[2J\033[H"
+    kill -- -$$
+}
+
+getkey() {
+    CURRENT_TTY_SETTINGS=$(stty -g)
+    stty -icanon -echo
+    head -c1
+    stty "$CURRENT_TTY_SETTINGS"
 }
 
 handleinput() {
-    case $1 in
-        d) : ;;
-        p) : ;;
-        s) : ;;
-        l) [ "$cursor" -lt "$items" ] && cursor=$((cursor + 1)) ;;
-        k) [ "$cursor" -gt 1 ] && cursor=$((cursor - 1)) ;;
-        n) quit ;;
+    sendargs() { transmission-remote "$@" > /dev/null; }
+    case $(getkey) in
+        h)
+            sendargs -t "$(cat $mark)" -S
+            ;;
+        l)
+            sendargs -t "$(cat $mark)" -s
+            ;;
+        d)
+            sendargs -t "$(cat $mark)" -r
+            setscreen
+            setheader
+            setfooter
+            drawtorrents
+            echo $(($(cat $cursor) - 1)) > "$cursor"
+            ;;
+        k)
+            [ "$(cat $cursor) " -gt 1 ] &&
+                echo $(($(cat $cursor) - 1)) > "$cursor"
+            ;;
+        j)
+            ITEMS=$(transmission-remote -l | sed '1d;$d' | wc -l)
+            [ "$(cat $cursor) " -lt "$ITEMS" ] &&
+                echo $(($(cat $cursor) + 1)) > "$cursor"
+            ;;
+        q) quit ;;
     esac
 }
 
-goto() {
-    printf "%b" "\033[${1};${2}H"
-}
-
-mark() {
-    printf "\033[7m"
-    echo "$@"
-    printf "\033[27m"
-}
-
-showui() {
-    goto 3 0
-    transmission-remote -l |
-        sed '1d;$d' |
+drawtorrents() {
+    goto 5 0
+    i=0
+    transmission-remote -l | sed '1d;$d' |
         while read -r line; do
             i=$((i + 1))
-            if [ "$i" = "$cursor" ]; then
+            if [ "$i" = "$(cat $cursor)" ]; then
                 mark "$line"
             else
-                echo "$line"
+                case $line in
+                    *\ \ 100%*) paint -g "$line" ;;
+                    *\ \ Stopped*) paint -r "$line" ;;
+                    *\ \ Idle*) paint -y "$line" ;;
+                    *) paint -w "$line" ;;
+                esac
             fi
         done
 }
 
-header() {
-    goto 1 "$((COLUMNS / 2 - 10))"
-    mark "$@"
-    printf "\n\n\n"
+paint() {
+    printf "\033["
+    case $1 in
+        -r) printf "31m" ;;
+        -g) printf "32m" ;;
+        -y) printf "33m" ;;
+        -w) printf "39m" ;;
+    esac
+    printf "%s\033[0m\n" "$2"
 }
 
-footer() {
-    goto "$((LINES - 1))" "$((COLUMNS / 2 - 10))"
-    mark "$@"
+mark() {
+    printf "\033[7m%s\033[27m\n" "$1"
+    echo "${1%% *}" > "$mark"
 }
+
+goto() { printf "\033[%s;%sH" "$1" "$2"; }
 
 setscreen() {
-    printf "%b" "$DISABLEWRAP$HIDECURSOR$CLEAR"
+    printf "\033[?7l\033[?25l\033[2J\033[H"
     LINES=$(stty size | cut -d' ' -f1)
     COLUMNS=$(stty size | cut -d' ' -f2)
+
+    printf "\033[2m"
+
+    goto 2 "$((COLUMNS / 2 - 10))"
+    echo "tide: Transmission Client"
+    goto 3 0
+    for i in $(seq "$COLUMNS"); do printf "%s" "-"; done
+    goto "$((LINES - 2))" 0
+    for i in $(seq "$COLUMNS"); do printf "%s" "-"; done
+    goto "$((LINES - 1))" "$((COLUMNS / 2 - 20))"
+    echo "h:Pause   j:Down   k:Up   l:Start   d:Delete   q:Quit"
+
+    printf "\033[0m"
+
 }
 
-trap 'setscreen' WINCH
+init() {
+    # if ! pidof transmission-daemon > /dev/null; then
+    #     transmission-daemon
+    #     sleep 1
+    # fi
+    echo 1 > "$cursor"
+}
 
-setscreen
-header Tide: Transmission TUI
-footer l:Down k:Up n:Quit
-while :; do
-    showui
-    handleinput "$(getkey)"
-done
+main() {
+    init
+    setscreen
+
+    trap 'quit' INT EXIT
+
+    while :; do
+        drawtorrents
+        sleep 1
+    done &
+
+    while :; do
+        handleinput
+        drawtorrents
+    done
+}
+main
