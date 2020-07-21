807254be487e7a177cdf26e824744bc7cf598428
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-07-21 06:59:46 +0600

    upload

--- tide.sh
+++ tide.sh
@@ -5,8 +5,8 @@
 # ENABLEWRAP="\033[?7h"
 # DISABLEWRAP="\033[?7l"
 
-ITEMS=0
-cursor=1
+cursor=/tmp/tide_cursor
+mark=/tmp/mark
 
 quit() {
     printf "\033[?25h\033[2J\033[H"
@@ -21,9 +21,27 @@ getkey() {
 }
 
 handleinput() {
+    sendargs() { transmission-remote "$@" > /dev/null; }
     case $(getkey) in
-        l) [ "$cursor" -lt "$ITEMS" ] && cursor=$((cursor + 1)) ;;
-        k) [ "$cursor" -gt 1 ] && cursor=$((cursor - 1)) ;;
+        p)
+            sendargs -t "$(cat $mark)" -S
+            ;;
+        s)
+            sendargs -t "$(cat $mark)" -s
+            ;;
+        d)
+            sendargs -t "$(cat $mark)" -r
+            ;;
+        k)
+            ITEMS=$(transmission-remote -l | sed '1d;$d' | wc -l)
+            [ "$(cat $cursor) " -gt 1 ] &&
+                echo $(($(cat $cursor) - 1)) > "$cursor"
+            ;;
+        l)
+            ITEMS=$(transmission-remote -l | sed '1d;$d' | wc -l)
+            [ "$(cat $cursor) " -lt "$ITEMS" ] &&
+                echo $(($(cat $cursor) + 1)) > "$cursor"
+            ;;
         n) quit ;;
     esac
 }
@@ -31,42 +49,22 @@ handleinput() {
 drawtorrents() {
     goto 5 0
     i=0
-    ITEMS=$(transmission-remote -l | sed '1d;$d' | wc -l)
     transmission-remote -l | sed '1d;$d' |
         while read -r line; do
             i=$((i + 1))
-            # ns "$i $cursor"
-            if [ "$i" = "$cursor" ]; then
+            if [ "$i" = "$(cat $cursor)" ]; then
                 mark "$line"
             else
                 echo "$line"
             fi
         done
-
-    # transmission-remote -l | sed '1d;$d' > "$TIDEFIFO" &
-    # while read -r line; do
-    #     i=$((i + 1))
-    #     # ns "$i $cursor"
-    #     if [ "$i" = "$cursor" ]; then
-    #         mark "$line"
-    #     else
-    #         echo "$line"
-    #     fi
-    # done < "$TIDEFIFO"
-
-    # done < /tmp/tide
 }
 
 mark() {
     printf "\033[7m"
-    echo "$@"
+    echo "$1"
     printf "\033[27m"
-}
-
-setscreen() {
-    printf "\033[?25l\033[2J\033[H"
-    LINES=$(stty size | cut -d' ' -f1)
-    COLUMNS=$(stty size | cut -d' ' -f2)
+    echo "${1%% *}" > "$mark"
 }
 
 goto() { printf "\033[%s;%sH" "$1" "$2"; }
@@ -89,11 +87,20 @@ setheader() {
     printf "\n\n\n"
 }
 
+setscreen() {
+    printf "\033[?25l\033[2J\033[H"
+    LINES=$(stty size | cut -d' ' -f1)
+    COLUMNS=$(stty size | cut -d' ' -f2)
+}
+
 init() {
+
     # if ! pidof transmission-daemon > /dev/null; then
     #     transmission-daemon
     #     sleep 1
     # fi
+
+    echo 1 > "$cursor"
 }
 
 main() {
@@ -104,6 +111,7 @@ main() {
     setfooter
 
     trap 'quit' INT EXIT
+    trap 'notify-send wc' WINCH
 
     while :; do
         drawtorrents
