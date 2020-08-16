d444bacf1cc6fe0f9b1f17a5ef75af9ed0689f0e
salman-abedin <salman104alvi@gmail.com>
2020-08-16 13:11:55 +0600

    Rewrote the shellscript in C

--- src/ui.c
+++ src/ui.c
@@ -15,16 +15,25 @@ void init_ui() {
    noecho();
    curs_set(0);
    getmaxyx(stdscr, height, width);
+
+   start_color();
+   use_default_colors();
+   init_pair(RUNNING_PAIR, COLOR_YELLOW, COLOR_BLACK);
+   init_pair(STOPPED_PAIR, COLOR_RED, -1);
+   init_pair(COMPLETED_PAIR, COLOR_GREEN, COLOR_BLACK);
+
    wwidth = width;
    wheight = height - 4;
    mark = start = 0;
 }
 
 void drawui() {
+   attron(A_DIM);
    mvprintw(0, (width - strlen(HEADER)) / 2, HEADER);
    mvprintw(height - 1, (width - strlen(FOOTER)) / 2, FOOTER);
    win = newwin(wheight, wwidth, 2, (width - wwidth) / 2);
    refresh();
+   attroff(A_DIM);
 }
 
 void _drawitems() {
@@ -37,9 +46,18 @@ void _drawitems() {
    end = count > wheight - 2 ? wheight - 2 : count;
 
    for (i = 1, j = start; j < end; ++i, ++j) {
-      if (i - 1 == mark) wattron(win, A_REVERSE);
+      if (i - 1 == mark) {
+         wattron(win, A_REVERSE);
+      } else if (strstr(items[j], "100%")) {
+         wattron(win, COLOR_PAIR(COMPLETED_PAIR));
+      } else if (strstr(items[j], "Stopped")) {
+         wattron(win, COLOR_PAIR(STOPPED_PAIR));
+      }
       mvwaddnstr(win, i, 1, items[j], wwidth - 2);
+
       wattroff(win, A_REVERSE);
+      wattroff(win, COLOR_PAIR(STOPPED_PAIR));
+      wattroff(win, COLOR_PAIR(COMPLETED_PAIR));
    }
 }
 
--- src/ui.h
+++ src/ui.h
@@ -4,6 +4,10 @@
 #define HEADER "tide: Tiny Transmission Client"
 #define FOOTER "h:Stop   j:Down   k:Up   l:Start   d:Delete   q:Quit"
 
+#define RUNNING_PAIR 1
+#define STOPPED_PAIR 2
+#define COMPLETED_PAIR 3
+
 void init_ui();
 void drawui();
 void handleinput();
