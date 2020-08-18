0a85b85496a15a60e5aa65222a2c82ee9e0711f8
salman-abedin <salman104alvi@gmail.com>
2020-08-18 17:45:19 +0600

    added remote daemon access support

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 static char* server[] = {
-    "192.168.0.101", /* remote address */
-                     /* "8022",          /1* remote port *1/ */
     /* comment out any one(but not both) of these for local usage */
+    "192.168.0.101", /* remote address */
+    /* "8022",          /1* remote port *1/ */
 };
