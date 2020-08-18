70448cc7afd0fda60354ee1f917927e70c454a91
salman-abedin <salman104alvi@gmail.com>
2020-08-18 18:07:28 +0600

    added remote daemon access support

--- config.h
+++ config.h
@@ -1,3 +1,3 @@
-#define REMOTE_USE 0 /* set this 1 for remote usage */
-#define REMOTE_IP "192.168.0.101"
-#define REMOTE_PORT "8080"
+#define REMOTE_USE 0              /* 1 = remote usage, 0 = local usage */
+#define REMOTE_IP "192.168.0.101" /* IP of the host running the daemon */
+#define REMOTE_PORT "8022"        /* PORT of the host running the daemon */
