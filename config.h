93c163a92cca8b395884e2c84ce910e64ac736bd
salman-abedin <salman104alvi@gmail.com>
2020-08-23 19:52:09 +0600

    added header & footer for torrent stats

--- config.h
+++ config.h
@@ -2,17 +2,23 @@
 //                             Bindings
 //==============================================================================
 
-/* vim bindings */
-#define TOR_UP 'k'
-#define TOR_DOWN 'j'
-#define TOR_START 'l'
-#define TOR_STOP 'h'
+/* /1* vim bindings *1/ */
+/* #define TOR_UP 'k' */
+/* #define TOR_DOWN 'j' */
+/* #define TOR_START 'l' */
+/* #define TOR_STOP 'h' */
 
 /* /1* normie bindings *1/ */
 /* #define TOR_UP KEY_UP */
 /* #define TOR_DOWN KEY_DOWN */
 /* #define TOR_START KEY_RIGHT */
 /* #define TOR_STOP KEY_LEFT */
+
+/* modified vim bindings */
+#define TOR_UP 'k'
+#define TOR_DOWN 'l'
+#define TOR_START ';'
+#define TOR_STOP 'j'
 
 #define TOR_DELETE 'd'
 #define TOR_QUIT 'q'
